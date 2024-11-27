acda56231f74f749bdd8b2d3fd0e4731eaf48874
Zain_ <129929845+MuhammadZainudin@users.noreply.github.com>
2024-11-27 16:18:05 +0700

    Update movie.dart

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -7,6 +7,7 @@ class Movie {
   final String? trailerUrl;
   double? rating;
   final String? releaseDate;
+  late final int releaseYear;
 
   Movie({
     required this.title,
@@ -17,20 +18,33 @@ class Movie {
     this.trailerUrl,
     this.rating,
     this.releaseDate,
-  });
+  }) {
+    releaseYear = _extractReleaseYear(releaseDate);
+  }
 
   factory Movie.fromJson(Map<String, dynamic> json) {
     return Movie(
-      title: json['title'],
-      coverUrl: json['cover_url'],
+      title: json['title'] ?? 'Unknown Title',
+      coverUrl: json['cover_url'] ?? '',
       overview: json['overview'],
       writtenBy: json['written_by'],
       directedBy: json['directed_by'],
       trailerUrl: json['trailer_url'],
       rating: json['imdb_rating'] != null
-          ? double.parse(json['imdb_rating'].toString())
+          ? double.tryParse(json['imdb_rating'].toString())
           : null,
       releaseDate: json['release_date'],
     );
+  }
+
+  int _extractReleaseYear(String? date) {
+    if (date == null || date.isEmpty) {
+      return 0;
+    }
+    try {
+      return int.parse(date.split('-')[0]);
+    } catch (e) {
+      return 0;
+    }
   }
 }
