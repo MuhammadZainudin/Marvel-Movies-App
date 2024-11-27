d18fd505e414978fe9c3ff636c5981334008d842
Zain_ <129929845+MuhammadZainudin@users.noreply.github.com>
2024-11-27 16:17:44 +0700

    Update login_page.dart

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,104 +1,134 @@
-import 'dart:io';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 import 'home_page.dart';
 
-class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key});
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
   @override
-  _ProfilePageState createState() => _ProfilePageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _ProfilePageState extends State<ProfilePage> {
-  File? _profileImage;
-
-  late Map<String, String> userDetails = {};
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    _loadUserDetails();
+    _clearInputFields();
   }
 
-  Future<void> _loadUserDetails() async {
-    final prefs = await SharedPreferences.getInstance();
-    setState(() {
-      userDetails = {
-        'username': prefs.getString('loggedInUsername') ?? 'Unknown User',
-        'firstName': prefs.getString('firstName') ?? '-',
-        'lastName': prefs.getString('lastName') ?? '-',
-        'gender': prefs.getString('gender') ?? '-',
-        'phone': prefs.getString('phone') ?? '-',
-        'address': prefs.getString('address') ?? '-',
-      };
-    });
+  void _clearInputFields() {
+    _usernameController.clear();
+    _passwordController.clear();
+  }
+
+  Future<bool> _checkAccount(String username, String password) async {
+    try {
+      const String apiUrl = 'http://localhost:3000/users';
+
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> users = jsonDecode(response.body);
+
+        final user = users.firstWhere(
+          (user) =>
+              user['username'] == username && user['password'] == password,
+          orElse: () => null,
+        );
+
+        if (user != null) {
+          final SharedPreferences prefs = await SharedPreferences.getInstance();
+          await prefs.setString('loggedInUsername', user['username']);
+          await prefs.setString('firstName', user['firstName']);
+          await prefs.setString('lastName', user['lastName']);
+          await prefs.setString('gender', user['gender']);
+          await prefs.setString('phone', user['phone']);
+          await prefs.setString('address', user['address']);
+          return true;
+        }
+      } else {
+        throw Exception('Failed to load users from API');
+      }
+    } catch (e) {
+      debugPrint('Error: $e');
+    }
+    return false;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => HomePage()),
-            );
-          },
-        ),
-      ),
-      body: userDetails.isEmpty
-          ? const Center(child: CircularProgressIndicator())
-          : Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ListView(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Marvel Movies', style: TextStyle(fontSize: 32)),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _usernameController,
+                decoration: const InputDecoration(hintText: 'Username'),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(hintText: 'Password'),
+                obscureText: true,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  GestureDetector(
-                    child: CircleAvatar(
-                      radius: 50,
-                      backgroundColor: Colors.grey,
-                      backgroundImage: _profileImage != null
-                          ? FileImage(_profileImage!)
-                          : null,
-                      child: _profileImage == null
-                          ? const Icon(Icons.person, size: 50)
-                          : null,
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  Text(
-                    userDetails['username'] ?? 'Unknown User',
-                    style: const TextStyle(
-                        fontSize: 24, fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 24),
-                  ListTile(
-                    title: const Text('Nama Depan'),
-                    subtitle: Text(userDetails['firstName'] ?? '-'),
-                  ),
-                  ListTile(
-                    title: const Text('Nama Belakang'),
-                    subtitle: Text(userDetails['lastName'] ?? '-'),
-                  ),
-                  ListTile(
-                    title: const Text('Jenis Kelamin'),
-                    subtitle: Text(userDetails['gender'] ?? '-'),
-                  ),
-                  ListTile(
-                    title: const Text('Nomor HP'),
-                    subtitle: Text(userDetails['phone'] ?? '-'),
-                  ),
-                  ListTile(
-                    title: const Text('Alamat'),
-                    subtitle: Text(userDetails['address'] ?? '-'),
+                  ElevatedButton(
+                    onPressed: () async {
+                      final username = _usernameController.text.trim();
+                      final password = _passwordController.text.trim();
+
+                      if (username.isEmpty || password.isEmpty) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content:
+                                Text('Username and Password cannot be empty!'),
+                          ),
+                        );
+                        _clearInputFields();
+                        return;
+                      }
+
+                      if (await _checkAccount(username, password)) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('Welcome, $username!'),
+                          ),
+                        );
+
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => HomePage()),
+                        ).then((_) => _clearInputFields());
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Invalid username or password!'),
+                          ),
+                        );
+                        _clearInputFields();
+                      }
+                    },
+                    child: const Text('Login'),
                   ),
                 ],
               ),
-            ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
